88b064007b25ce9ba496c21a89e5bf2ceb34d908
b3liott <eliott.georges8@gmail.com>
2024-10-05 14:59:42 +0200

    komit

--- settings.py
+++ settings.py
@@ -4,10 +4,10 @@ PACKET_SIZE = 1024  # taille d'un paquet
 MAX_STORAGE = 1024 * 1000  # taille de stockage max par edge
 FREQ = 2  # secondes
 
+
 # MQTT
 BROKER_ADDRESS = "localhost"
 BROKER_PORT = 1883
-TOPIC = "test/topic"
 
 # Récupération des stats des workers
 WORKERS_PING = ["wokers/pone", "wokers/ptwo", "wokers/pthree"]
