41bd6be619ef3f660fc06be7f2c11e081f8d1f17
b3liott <eliott.georges8@gmail.com>
2024-10-17 17:40:17 +0200

    ok

--- modules/allocator/LB.py
+++ modules/allocator/LB.py
@@ -28,45 +28,6 @@ class LB:
         self.m = len(edges)
         range_n = range(self.n)
         range_m = range(self.m)
-        indx = [(i, j) for i in range_n for j in range_m]
-
-        # Modèle
-        self.model = Model(name=name)
-        x_dict = self.model.binary_var_dict(indx, name="x")
-
-        # ---- Contraintes ----
-        # Allocation
-        for i in range_n:
-            self.model.add_constraint(self.model.sum(x_dict[i, j] for j in range_m) == 1)
-
-        # Capacité de stockage des fog nodes
-        for j in range_m:
-            self.model.add_constraint(self.model.sum(
-                PACKET_SIZE * x_dict[i, j] for i in
-                range_n) <= edges[j].remainingStorage
-            )
-
-        # Capacité cpu
-        for i in range_n:
-            for j in range_m:
-                self.model.add_constraint(edges[j].load + x_dict[i,j] <= 80)
-        # ----------------------------
-
-        # ---- Fonction objective ----
-        W = 1
-        self.model.minimize(
-            self.model.sum(
-                self.model.sum(
-                    edges[j].load * x_dict[i, j] for j in range_m
-                ) for i in range_n
-            )
-            + W*self.model.sum(
-                self.model.sum(
-                    x_dict[i, j] for i in range_n
-                ) **2 for j in range_m
-            )
-        )
-        # ----------------------------
 
 
     def update_edges(self, edges: list[Edge]):
@@ -75,15 +36,10 @@ class LB:
         for edge in edges:
             logger.info(edge)
 
+
     def solve(self) -> dict[str, int]:
         try:
-            self.model.solve(log_output=False)
-            x = self.model.solution
             allocation_dict = {}
-            for k, v in x.as_dict().items():
-                if v == 1.0:
-                    target = getEdgeNameFromID(int(k.name.split('_')[2]), self.edges)
-                    allocation_dict[target] = 1 if target not in allocation_dict else allocation_dict[target] + 1
 
             return allocation_dict
         except TypeError or AttributeError:
@@ -93,8 +49,3 @@ class LB:
             logger.error('Erreur LB: ', str(e))
             return {}
 
-    def print_information(self):
-        print(str(self.model.print_information()))
-
-    def print_solution(self):
-        print(str(self.model.print_solution()))
